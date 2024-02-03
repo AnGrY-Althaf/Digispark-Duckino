9349b678adddac886b4c119389f6c456cef772fa
Althaf Shajahan <156542448+AnGrY-Althaf@users.noreply.github.com>
2024-02-04 00:28:17 +0530

    Add files via upload

---
+++ Scripts/windowsXP.ino
@@ -0,0 +1,67 @@
+// Converted using digiQuack !
+
+#include "DigiKeyboard.h"
+void setup() {
+	DigiKeyboard.sendKeyStroke(0);
+	// This Script is Created By AnGrY 
+	DigiKeyboard.delay(1000);
+	DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+	DigiKeyboard.delay(500);
+	DigiKeyboard.print("cmd");
+	DigiKeyboard.delay(500);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(500);
+	DigiKeyboard.print("color a");
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(300);
+	DigiKeyboard.print("dir/s");
+	DigiKeyboard.delay(100);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+	DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+	DigiKeyboard.delay(500);
+	DigiKeyboard.print("cmd");
+	DigiKeyboard.delay(300);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+	DigiKeyboard.print("color a");
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(300);
+	DigiKeyboard.print("dir/s");
+	DigiKeyboard.delay(100);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+	DigiKeyboard.delay(300);
+	DigiKeyboard.print("PowerShell -w hidden -Command \"Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('THANK YOU FOR PLUGIN ME IN ','HACKED')\"");
+	DigiKeyboard.delay(300);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(3000);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(100);
+	DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+	DigiKeyboard.delay(1000);
+	DigiKeyboard.print("chrome");
+	DigiKeyboard.delay(500);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(750);
+	DigiKeyboard.print("https://geekprank.com/matrix-code-rain/");
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(1000);
+	DigiKeyboard.sendKeyStroke(KEY_F11);
+	DigiKeyboard.delay(5000);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(23000);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(4000);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(25000);
+	DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+	DigiKeyboard.delay(300);
+	DigiKeyboard.print("chrome https://geekprank.com");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(500);
+}
+
+void loop() {}
